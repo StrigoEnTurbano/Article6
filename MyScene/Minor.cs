7723f648714080491c63baca0cc6d66659b3f8fd
Зайнуллин Ратмир <zaynullinratmir@yandex.ru>
2024-04-09 01:18:42 +0500

    Создал репозиторий для статьи.

---
+++ MyScene/Minor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public partial class Minor : GodotFSharp.Core.MyScene.Minor
+{
+    public static void Initialize()
+    {
+        GodotFSharp.Core.MyScene.Minor.Factory = () => new Minor();
+    }
+
+    public override void _Draw() => base._Draw();
+
+    public override void _Process(double delta) => base._Process(delta);
+}
